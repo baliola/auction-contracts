48002b3f4f2f37587cb81c4801992d92f7a08ed6
zian <mynameiszian@gmail.com>
2022-11-07 16:00:44 +0800

    make the from address explicit in kepeng migration

--- migrations/1-kepeng.migration.js
+++ migrations/1-kepeng.migration.js
@@ -1,5 +1,6 @@
-const { kepengArtifact } = require("../utils/utils")
+const { kepengArtifact, getDeployerWallet } = require("../utils/utils")
 
-module.exports = async function (deployer) {
-    await deployer.deploy(kepengArtifact)
+module.exports = async function (deployer, network, accounts) {
+    const deployerAddress = getDeployerWallet(accounts)
+    await deployer.deploy(kepengArtifact, { from: deployerAddress })
 }
