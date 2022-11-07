2b1ca27f75a2e5cb002e00b474bdd966bf86faa0
zian <mynameiszian@gmail.com>
2022-11-07 10:01:14 +0800

    added contract definition in utils

--- test/utils/utils.js
+++ test/utils/utils.js
@@ -1,2 +1,10 @@
 const auction721 = artifacts.require("Auction721");
 const auction1155 = artifacts.require("Auction1155");
+const auctionManager721 = artifacts.require("AuctionManager721");
+const auctionManager1155 = artifacts.require("AuctionManager1155");
+const nft721 = artifacts.require("Dummy721");
+const nft1155 = artifacts.require("Dummy1155");
+const fixedPriceAuction = artifacts.require("AuctionFixedPrice1155");
+const fixedPriceAuctionManager = artifacts.require("FixedPriceAuctionManager1155");
+const kepeng = artifacts.require("KEPENG");
+
