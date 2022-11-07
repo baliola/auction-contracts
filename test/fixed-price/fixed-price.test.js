a6297c41f0147f11289bb12d470692066e3ed4b6
zian <mynameiszian@gmail.com>
2022-11-07 14:11:25 +0800

    added test for changing manager

--- test/fixed-price/fixed-price.test.js
+++ test/fixed-price/fixed-price.test.js
@@ -1,10 +1,28 @@
 const { assert } = require("chai")
 const truffleAssert = require("truffle-assertions")
 const helpers = require("../helpers/truffle-time-helpers")
-const { fixedPriceAuctionManager } = require("../../utils/utils");
+const { fixedPriceAuctionManager, getManagerWallet, getUserWallets, getDeployedContracts, fixedPriceAuctionManagerArtifact } = require("../../utils/utils");
 
 contract(fixedPriceAuctionManager, async (accounts) => {
-    it("lmao test", () => {
-        assert.equal(1, 1);
+    it("should change manager", async () => {
+        const check = Array.isArray(accounts)
+
+        const currentManager = getManagerWallet(accounts)
+        const userAccounts = getUserWallets(accounts)
+        const newManager = userAccounts[0]
+        const newBaliolaWallet = userAccounts[1]
+
+        const managerError = "only manager can call this function"
+
+        const contract = await getDeployedContracts(fixedPriceAuctionManagerArtifact)
+
+        const changeManager = await contract.changeManager(newManager, { from: currentManager });
+
+        try {
+            const tryChangeManager = await contract.changeManager(currentManager, { from: currentManager })
+        } catch (error) {
+            assert.strictEqual(error.reason, managerError)
+        }
+
     })
 })
