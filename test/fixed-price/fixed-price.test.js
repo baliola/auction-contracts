b9604a36b4e3b25cc1db64195353132db8d244b0
zian <mynameiszian@gmail.com>
2022-11-07 15:52:05 +0800

    added test for creating auction

--- test/fixed-price/fixed-price.test.js
+++ test/fixed-price/fixed-price.test.js
@@ -1,12 +1,12 @@
 const { assert } = require("chai")
 const truffleAssert = require("truffle-assertions")
 const helpers = require("../helpers/truffle-time-helpers")
-const { fixedPriceAuctionManager, getManagerWallet, getUserWallets, getDeployedContracts, fixedPriceAuctionManagerArtifact } = require("../../utils/utils");
+const { fixedPriceAuctionManager, getManagerWallet, getUserWallets, getDeployedContracts, fixedPriceAuctionManagerArtifact, nft1155, nft1155Artifact, getDeployerWallet } = require("../../utils/utils");
+
+// IMPORTANT : always use assert.strictEqual when asserting condition
 
 contract(fixedPriceAuctionManager, async (accounts) => {
     it("should change manager", async () => {
-        const check = Array.isArray(accounts)
-
         const currentManager = getManagerWallet(accounts)
         const userAccounts = getUserWallets(accounts)
         const newManager = userAccounts[0]
@@ -25,4 +25,27 @@ contract(fixedPriceAuctionManager, async (accounts) => {
         }
 
     })
+
+    it("should create auction", async () => {
+        const price = 1e4
+        const deployer = getDeployerWallet(accounts)
+        const userWallets = getUserWallets(accounts)
+        const user = userWallets[0]
+        const nft1155Contract = await getDeployedContracts(nft1155Artifact)
+        const nftTokenId = 0
+        const nftAmount = 1
+        const auctionManager = await getDeployedContracts(fixedPriceAuctionManagerArtifact)
+        const initAuctionsList = await auctionManager.getAuctions()
+
+        // mint nft for auction and set approval for auction manager 
+        await nft1155Contract.mint(user, nftAmount, "0x", { from: deployer });
+        await nft1155Contract.setApprovalForAll(auctionManager.address, true, { from: user })
+
+        // create auction
+        await auctionManager.createAuction(price, nft1155Contract.address, nftTokenId, nftAmount, user, { from: user })
+        const currentAuctionList = await auctionManager.getAuctions()
+
+        assert.strictEqual(currentAuctionList.length, initAuctionsList.length + 1)
+    })
+
 })
