1612ce10d969a9cf8a79b519b5951a52fbd04298
zian <mynameiszian@gmail.com>
2022-11-07 10:13:59 +0800

    added comments in utils

--- test/fixed-price/fixed-price.test.js
+++ test/fixed-price/fixed-price.test.js
@@ -2,4 +2,3 @@ import { assert } from 'chai';
 import truffleAssert from "truffle-assertions"
 import * as helpers from "../helpers/truffle-time-helpers"
 
-const kepeng = artifacts.require("KEPENG");
--- test/utils/utils.js
+++ test/utils/utils.js
@@ -1,13 +1,30 @@
-const auction721 = artifacts.require("Auction721");
-const auction1155 = artifacts.require("Auction1155");
-const auctionManager721 = artifacts.require("AuctionManager721");
-const auctionManager1155 = artifacts.require("AuctionManager1155");
-const nft721 = artifacts.require("Dummy721");
-const nft1155 = artifacts.require("Dummy1155");
-const fixedPriceAuction = artifacts.require("AuctionFixedPrice1155");
-const fixedPriceAuctionManager = artifacts.require("FixedPriceAuctionManager1155");
-const kepeng = artifacts.require("KEPENG");
+// contract abi/artifacts 
+const auction721Artifact = artifacts.require("Auction721");
+const auction1155Artifact = artifacts.require("Auction1155");
+const auctionManager721Artifact = artifacts.require("AuctionManager721");
+const auctionManager1155Artifact = artifacts.require("AuctionManager1155");
+const nft721Artifact = artifacts.require("Dummy721");
+const nft1155Artifact = artifacts.require("Dummy1155");
+const fixedPriceAuctionArtifact = artifacts.require("AuctionFixedPrice1155");
+const fixedPriceAuctionManagerArtifact = artifacts.require("FixedPriceAuctionManager1155");
+const kepengArtifact = artifacts.require("KEPENG");
 
+// contract name definitions
+const auction721 = "Auction721"
+const auction1155 = "Auction1155"
+const auctionManager721 = "AuctionManager721"
+const auctionManager1155 = "AuctionManager1155"
+const nft721 = "Dummy721"
+const nft1155 = "Dummy1155"
+const fixedPriceAuction = "AuctionFixedPrice1155"
+const fixedPriceAuctionManager = "FixedPriceAuctionManager1155"
+const kepeng = "KEPENG"
+
+/**
+ * 
+ * @param {any} contract use the contract abi constants defined in utils.js
+ * @returns contract instance
+ */
 async function deploy(contract) {
     const contract = await contract.deployed();
 
@@ -21,8 +38,17 @@ export {
     auctionManager1155,
     nft721,
     nft1155,
-    fixedPriceAuctionManager,
     fixedPriceAuction,
+    fixedPriceAuctionManager,
     kepeng,
+    auction721Artifact,
+    auction1155Artifact,
+    auctionManager721Artifact,
+    auctionManager1155Artifact,
+    nft721Artifact,
+    nft1155Artifact,
+    fixedPriceAuctionManagerArtifact,
+    fixedPriceAuctionArtifact,
+    kepengArtifact,
     deploy
 }
