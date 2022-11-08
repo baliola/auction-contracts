573f31c69efa93fcbf20803154f1afb1323ca255
zian <mynameiszian@gmail.com>
2022-11-08 09:45:56 +0800

    added function for approving kepeng

--- test/helpers/auction.helpers.js
+++ test/helpers/auction.helpers.js
@@ -1,4 +1,4 @@
-const { getDeployerWallet, getUserWallets, getDeployedContracts, nft1155Artifact, fixedPriceAuctionManagerArtifact } = require("../../utils/utils")
+const { getDeployerWallet, getUserWallets, getDeployedContracts, nft1155Artifact, fixedPriceAuctionManagerArtifact, instantiateAuctionContractAtAddress, defaultNftAMount, kepengArtifact } = require("../../utils/utils")
 
 const defaultAuctionIndex = 1;
 /**
@@ -10,15 +10,30 @@ async function createDummyFixedPrice1155Auction(accounts, nftSellerAddress) {
     const deployer = getDeployerWallet(accounts)
     const nft1155Contract = await getDeployedContracts(nft1155Artifact)
     const nftTokenId = 0
-    const nftAmount = 1
     const auctionManager = await getDeployedContracts(fixedPriceAuctionManagerArtifact)
 
     // mint nft for auction and set approval for auction manager 
-    await nft1155Contract.mint(nftSellerAddress, nftAmount, "0x", { from: deployer });
+    await nft1155Contract.mint(nftSellerAddress, defaultNftAMount, "0x", { from: deployer });
     await nft1155Contract.setApprovalForAll(auctionManager.address, true, { from: nftSellerAddress })
 
     // create auction
     await auctionManager.createAuction(price, nft1155Contract.address, nftTokenId, nftAmount, nftSellerAddress, { from: nftSellerAddress })
+}
+
+async function buyNftFromAuction(user) {
+    const auctionManager = getDeployedContracts(fixedPriceAuctionManagerArtifact)
+    const fee = getDefaultMarkupFee()
+
+    await auctionManager.buy(defaultAuctionIndex, fee, defaultNftAMount)
+
+}
+
+async function approveFixedPriceAuctionManager(address, amount) {
+    const auctionManager = getDeployedContracts(fixedPriceAuctionManagerArtifact)
+    const kepengContract = getDeployedContracts(kepengArtifact)
+
+    await kepengContract.approve(auctionManager.address, amount)
+    return;
 }
 
 function getDefaultMarkupFee() {
