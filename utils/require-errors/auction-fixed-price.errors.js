e8cbb7b7f6bd8730761d630c6514faa352a8edb1
zian <mynameiszian@gmail.com>
2022-11-08 22:14:54 +0800

    added special auction  errors definitions

--- utils/require-errors/auction-fixed-price.errors.js
+++ utils/require-errors/auction-fixed-price.errors.js
@@ -3,3 +3,15 @@ const OUT_OF_SUPPLY = "out of supply! no nft is being selled!"
 const NOT_ENOUGH_NFT = "not enough available nft!"
 const CAN_ONLY_BUY_IF_AUCTION_OPEN = "can only buy nft when auction is open!"
 const CAN_ONLY_BUY_IF_FEE_IS_CORRECT = "can only buy if the fee is correct!"
+
+const SPECIAL_FIXED_PRICE_AUCTION_ERRORS = {
+    ONLY_CREATOR_CAN_REFILL,
+    OUT_OF_SUPPLY,
+    NOT_ENOUGH_NFT,
+    CAN_ONLY_BUY_IF_AUCTION_OPEN,
+    CAN_ONLY_BUY_IF_FEE_IS_CORRECT,
+}
+
+module.exports = {
+    SPECIAL_FIXED_PRICE_AUCTION_ERRORS
+}
