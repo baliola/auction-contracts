c1fa4675044a6aba4f180334da9b6669a0785227
zian <mynameiszian@gmail.com>
2022-11-08 22:13:06 +0800

    added common auction manager errors definitions

--- utils/require-errors/common/common-auction-manager.errors.js
+++ utils/require-errors/common/common-auction-manager.errors.js
@@ -3,10 +3,13 @@ const DIRECT_BUY_PRICE_MUST_NOT_BE_0 = "direct buy price must be greater than 0"
 const START_PRICE_MUST_BE_LESS_THAN_DIRECT_BUY_PRICE = "start price must be less than the direct buy price"
 const END_TIME_MUST_BE_GREATER_THAN_12_HOURS = "must be greater than 12 hours"
 
-
-module.exports = {
+const COMMON_MANAGER_ERRORS = {
     ONLY_MANAGER,
     DIRECT_BUY_PRICE_MUST_NOT_BE_0,
     START_PRICE_MUST_BE_LESS_THAN_DIRECT_BUY_PRICE,
     END_TIME_MUST_BE_GREATER_THAN_12_HOURS,
+}
+
+module.exports = {
+    COMMON_MANAGER_ERRORS
 }
