6875844fa99ca051a956dfc338a18fe1a2549b8c
zian <mynameiszian@gmail.com>
2022-11-09 21:51:58 +0800

    fixed wrong bindings

--- test/helpers/bindings/fixed-auction-manager-1155.js
+++ test/helpers/bindings/fixed-auction-manager-1155.js
@@ -1,7 +1,6 @@
-const { fixedPriceAuctionArtifact } = require("../../../utils/utils");
-const { AuctionFixedPrice1155 } = require("./auction-fixed-price-1155");
+import { AuctionFixedPrice1155 } from "./auction-fixed-price-1155";
 
-class FixedPriceAuctionManager1155 {
+export default class FixedPriceAuctionManager1155 {
   contractInstance
   contractAddress
 
@@ -10,18 +9,18 @@ class FixedPriceAuctionManager1155 {
     this.contractAddress = contract.address;
   }
 
-  async auctionsByAddress(argv1, fromAddress) {
-    const tx = await this.contractInstance.auctionsByAddress(argv1, { from: fromAddress });
+  async auctionsByAddress(argv1) {
+    const tx = await this.contractInstance.auctionsByAddress(argv1);
     return tx;
   }
 
-  async auctionsByIndex(argv1, fromAddress) {
-    const tx = await this.contractInstance.auctionsByIndex(argv1, { from: fromAddress });
+  async auctionsByIndex(argv1) {
+    const tx = await this.contractInstance.auctionsByIndex(argv1);
     return tx;
   }
 
-  async auctionsUser(argv1, argv2, fromAddress) {
-    const tx = await this.contractInstance.auctionsUser(argv1, argv2, { from: fromAddress });
+  async auctionsUser(argv1, argv2) {
+    const tx = await this.contractInstance.auctionsUser(argv1, argv2);
     return tx;
   }
 
@@ -40,50 +39,50 @@ class FixedPriceAuctionManager1155 {
     return tx;
   }
 
-  async changeManager(newManager, fromAddress) {
-    const tx = await this.contractInstance.changeManager(newManager, { from: fromAddress });
+  async changeManager(newManager) {
+    const tx = await this.contractInstance.changeManager(newManager);
     return tx;
   }
 
-  async changeBaliolaWallet(newBaliolaWallet, fromAddress) {
-    const tx = await this.contractInstance.changeBaliolaWallet(newBaliolaWallet, { from: fromAddress });
+  async changeBaliolaWallet(newBaliolaWallet) {
+    const tx = await this.contractInstance.changeBaliolaWallet(newBaliolaWallet);
     return tx;
   }
 
-  async getUserAuction(user, fromAddress) {
-    const tx = await this.contractInstance.getUserAuction(user, { from: fromAddress });
+  async getUserAuction(user) {
+    const tx = await this.contractInstance.getUserAuction(user);
     return tx;
   }
 
-  async placeBid(auctionId, transactionFee, fromAddress) {
-    const tx = await this.contractInstance.placeBid(auctionId, transactionFee, { from: fromAddress });
+  async buy(auctionId, transactionFee, amount) {
+    const tx = await this.contractInstance.buy(auctionId, transactionFee, amount);
     return tx;
   }
 
-  async createAuction(_endTime, _directBuyAuction, _directBuyPrice, _startPrice, _nftAddress, _tokenId, _nftAmount, _nftSeller, fromAddress) {
-    const tx = await this.contractInstance.createAuction(_endTime, _directBuyAuction, _directBuyPrice, _startPrice, _nftAddress, _tokenId, _nftAmount, _nftSeller, { from: fromAddress });
+  async refill(auctionId, amount) {
+    const tx = await this.contractInstance.refill(auctionId, amount);
     return tx;
   }
 
-  async getAuctions(fromAddress) {
-    const tx = await this.contractInstance.getAuctions({ from: fromAddress });
+  async createAuction(price, _nftAddress, _tokenId, _nftAmount, _nftSeller) {
+    const tx = await this.contractInstance.createAuction(price, _nftAddress, _tokenId, _nftAmount, _nftSeller);
     return tx;
   }
 
+  async getAuctions() {
+    const tx = await this.contractInstance.getAuctions();
+    return tx;
+  }
   /**
-   * 
-   * @param {string} address 
-   * @returns {Promise<AuctionFixedPrice1155>} auction fixed price 1155 contract instance
-   */
+ * 
+ * @param {string} address 
+ * @returns {Promise<AuctionFixedPrice1155>} auction fixed price 1155 contract instance
+ */
   async inferAuction(address) {
     const instance = await fixedPriceAuctionArtifact.at(address)
     const contract = new AuctionFixedPrice1155(instance)
-
     return contract
   }
 
 
-}
-module.exports = {
-  FixedPriceAuctionManager1155
 }
