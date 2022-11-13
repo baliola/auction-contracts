c2a969389beb5355da8f8b5039301249f7e7bacf
zian <mynameiszian@gmail.com>
2022-11-13 20:23:27 +0800

    added documentation

--- test/helpers/truffle-time.helpers.js
+++ test/helpers/truffle-time.helpers.js
@@ -2,12 +2,20 @@
 
 class TimeHelper {
 
-    advanceTime(time) {
+    /**
+     * NOTE : this is a low level api, you may not need this. use `advanceTimeAndBlock` instead.
+     * 
+     * this method will artifically advance time in seconds
+     * 
+     * @param {number} timeInSeconds time in seconds
+     * @returns {void}
+     */
+    async advanceTime(timeInSeconds) {
         return new Promise((resolve, reject) => {
             web3.currentProvider.send({
                 jsonrpc: '2.0',
                 method: 'evm_increaseTime',
-                params: [time],
+                params: [timeInSeconds],
                 id: new Date().getTime()
             }, (err, result) => {
                 if (err) { return reject(err) }
@@ -16,7 +24,13 @@ class TimeHelper {
         })
     }
 
-    advanceBlock() {
+    /**
+     * NOTE : this is a low level api, you may not need this. use `advanceTimeAndBlock` instead.
+     *  
+     * this method will mine blocks that hasn't been mined.
+     * @returns {Promise<void>}
+     */
+    async advanceBlock() {
         return new Promise((resolve, reject) => {
             web3.currentProvider.send({
                 jsonrpc: '2.0',
@@ -31,8 +45,14 @@ class TimeHelper {
         })
     }
 
-    async advanceTimeAndBlock(time) {
-        await advanceTime(time)
+    /**
+     * this method will advance time in seconds and then mine the blocks based on the time passed.
+     * 
+     * @param {number} timeInSeconds time in seconds.
+     * @returns {Promise<void>}
+     */
+    async advanceTimeAndBlock(timeInSeconds) {
+        await advanceTime(timeInSeconds)
         await advanceBlock()
         return Promise.resolve(web3.eth.getBlock('latest'))
     }
