26b02122ae0a46df9f23253afe85891467f8952d
zian <mynameiszian@gmail.com>
2022-11-13 20:17:04 +0800

    wrap functions as method

--- test/helpers/truffle-time.helpers.js
+++ test/helpers/truffle-time.helpers.js
@@ -1,40 +1,44 @@
-function advanceTime(time) {
-    return new Promise((resolve, reject) => {
-        web3.currentProvider.send({
-            jsonrpc: '2.0',
-            method: 'evm_increaseTime',
-            params: [time],
-            id: new Date().getTime()
-        }, (err, result) => {
-            if (err) { return reject(err) }
-            return resolve(result)
+
+
+class TimeHelper {
+
+    advanceTime(time) {
+        return new Promise((resolve, reject) => {
+            web3.currentProvider.send({
+                jsonrpc: '2.0',
+                method: 'evm_increaseTime',
+                params: [time],
+                id: new Date().getTime()
+            }, (err, result) => {
+                if (err) { return reject(err) }
+                return resolve(result)
+            })
         })
-    })
-}
+    }
 
-function advanceBlock() {
-    return new Promise((resolve, reject) => {
-        web3.currentProvider.send({
-            jsonrpc: '2.0',
-            method: 'evm_mine',
-            id: new Date().getTime()
-        }, (err, result) => {
-            if (err) { return reject(err) }
-            const newBlockHash = web3.eth.getBlock('latest').hash
+    advanceBlock() {
+        return new Promise((resolve, reject) => {
+            web3.currentProvider.send({
+                jsonrpc: '2.0',
+                method: 'evm_mine',
+                id: new Date().getTime()
+            }, (err, result) => {
+                if (err) { return reject(err) }
+                const newBlockHash = web3.eth.getBlock('latest').hash
 
-            return resolve(newBlockHash)
+                return resolve(newBlockHash)
+            })
         })
-    })
+    }
+
+    async advanceTimeAndBlock(time) {
+        await advanceTime(time)
+        await advanceBlock()
+        return Promise.resolve(web3.eth.getBlock('latest'))
+    }
 }
 
-async function advanceTimeAndBlock(time) {
-    await advanceTime(time)
-    await advanceBlock()
-    return Promise.resolve(web3.eth.getBlock('latest'))
-}
 
 module.exports = {
-    advanceTime,
-    advanceBlock,
-    advanceTimeAndBlock,
+    TimeHelper
 }
