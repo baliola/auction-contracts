7b2ee9b47e01b953c57579aff120dbb8d59f157a
zian <mynameiszian@gmail.com>
2022-12-08 15:31:22 +0800

    adjust some error messages

--- utils/require-errors/auction.errors.js
+++ utils/require-errors/auction.errors.js
@@ -1,5 +1,5 @@
 const AUCTION_MUST_END_1155 =
-  "The auction must be ended by either a direct buy, by creator, or timeout";
+  "The auction must be ended by either a direct buy, by seller, or timeout";
 
 const SPECIAL_AUCTION_1155_ERRORS = {
   AUCTION_MUST_END_1155,
